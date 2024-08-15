fb72734db1d69688a40e7228de8a4dddc0839dff
Vimal Vijay <vimalvijay82@gmail.com>
2024-08-15 18:14:34 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,57 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path="/" element={<MainLayout />}>,
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='*' element={<NotFoundPage />} />
-  </Route>
-)
-);
 
 const App = () => {
+  // Add a new job
+  const addJob =async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  };
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    return;
+  };
+
+  //update Job
+  const updateJob = async (updatedJob) => {
+    const res = await fetch(`/api/jobs/${updatedJob.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedJob)
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>,
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+      <Route path='/jobs/:id' element={<JobPage  deleteJob={deleteJob} />} loader={jobLoader} />
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader} />
+    </Route>
+  )
+  );
+
   return <RouterProvider router={router} />
 };
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,6 @@
 import Navbar from "../components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {Outlet} from 'react-router-dom';
 
 const MainLayout = () => {
@@ -6,6 +8,7 @@ const MainLayout = () => {
         <>
             <Navbar />
             <Outlet />
+            <ToastContainer />
         </>
     )
 }
